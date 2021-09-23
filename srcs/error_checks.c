6413fd8901560d6896a238f6e80760e6748ab443
Julia Mendes <juliamendesc@gmail.com>
2021-09-23 18:49:47 +0100

    changes to intmin intmax detection and multiple 0 before number

--- srcs/error_checks.c
+++ srcs/error_checks.c
@@ -37,17 +37,6 @@ int	duplicate_validation(char **args, int size)
 	return (f);
 }
 
-int	digit_bigger_than_min_max_int(char *argv)
-{
-	if (argv[0] == '-' && (ft_strlen(argv) > 11 || (ft_strlen(argv) == 11
-				&& ft_memcmp(argv, "-2147483648", 11) > 0)))
-		return (1);
-	if (argv[0] != '-' && ((ft_strlen(argv) == 10
-				&& ft_memcmp(argv, "2147483647", 11) > 0) || ft_strlen(argv) > 10))
-		return (1);
-	return (0);
-}
-
 /*
 ** Check for errors:
 ** minimum of 2 arguments;
@@ -65,10 +54,10 @@ int	error_handling(int size, char **argv)
 		ft_exit_ps("Wrong number of arguments.\nPlease use: ./push_swap \"list of numbers\"\n", -1);
 	while (++i < size)
 	{
+		if (ft_atoll(argv[i]) > 2147483647 || ft_atoll(argv[i]) < -2147483648)
+			ft_exit_ps("Numbers out of integer range\n", -1);
 		if (ft_isstringdigit(argv[i]) == 0)
 			ft_exit_ps("Numbers have to be a digit\n", -1);
-		if (digit_bigger_than_min_max_int(argv[i]) == 1)
-			ft_exit_ps("Numbers out of integer range\n", -1);
 	}
 	if (duplicate_validation(argv, size) == 1)
 			ft_exit_ps("There cannot be duplicate numbers\n", -1);
